6349ff89d159135d2deab5e2da637b258fe72689
Alex Ghiban <drew7721@gmail.com>
2019-12-20 16:58:31 -0500

    Init module.

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'JustinKase_CanadaPostShipping',
+    __DIR__
+);
